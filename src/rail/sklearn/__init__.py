2a41310b2ac74da54705c233cc25f1fe7f9d97b1
taiwithers <59430904+taiwithers@users.noreply.github.com>
2025-11-27 15:16:08 -0500

    Formatting

--- src/rail/sklearn/__init__.py
+++ src/rail/sklearn/__init__.py
@@ -1,6 +1,6 @@
-from ._version import __version__
-
 from rail.estimation.algos.k_nearneigh import *
-from rail.estimation.algos.sklearn_neurnet import *
 from rail.estimation.algos.nz_dir import *
 from rail.estimation.algos.random_forest import *
+from rail.estimation.algos.sklearn_neurnet import *
+
+from ._version import __version__
